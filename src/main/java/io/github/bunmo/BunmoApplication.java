7fac6cdcba737a7763b3ca859db53462ba663031
youable <youable.framios@gmail.com>
2025-12-22 09:43:43 +0900

    first commit

---
+++ src/main/java/io/github/bunmo/BunmoApplication.java
@@ -0,0 +1,13 @@
+package io.github.bunmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BunmoApplication.class, args);
+    }
+
+}
