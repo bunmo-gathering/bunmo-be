0c03b2e451525595d4392a6837a6e508f3058fe5
youable <youable.framios@gmail.com>
2026-01-05 16:08:06 +0900

    feat(common): code review feedback apply

--- src/main/java/io/github/bunmo/common/web/GlobalControllerAdvice.java
+++ src/main/java/io/github/bunmo/common/web/GlobalControllerAdvice.java
@@ -1,15 +1,18 @@
 package io.github.bunmo.common.web;
 
 import io.github.bunmo.common.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<?> handleCustomException(CustomException e) {
+        log.debug(e.getMessage());
         return ResponseEntity
                 .status(e.errorCode().statusCode())
                 .body(ApiResponse.error(e.errorCode()));
