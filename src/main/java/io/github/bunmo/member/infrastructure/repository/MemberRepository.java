d4aa793a3badbc81d582bdaf0232976b92d78129
youable <youable.framios@gmail.com>
2026-01-31 18:09:14 +0900

    fix(member): 누락된 Repository 코드

--- src/main/java/io/github/bunmo/member/infrastructure/repository/MemberRepository.java
+++ src/main/java/io/github/bunmo/member/infrastructure/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package io.github.bunmo.member.infrastructure.repository;
 
 import io.github.bunmo.member.infrastructure.domain.Member;
+import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUuid(String uuid);
 }
