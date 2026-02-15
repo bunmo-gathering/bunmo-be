693c026f2018b9c12bb6d8be6a5c449b23fa0e59
youable <youable.framios@gmail.com>
2026-02-15 17:21:59 +0900

    feat: init schema ddl 쿼리 추가

---
+++ db/mysql/init/schema.sql
@@ -0,0 +1,83 @@
+-- bunmo.`member` definition
+
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `active_type` enum('ACTIVE','BANNED','INACTIVE','PENDING','WITHDRAWAL') NOT NULL,
+  `created_at` datetime(6) NOT NULL,
+  `gathering_open_count` int NOT NULL,
+  `x` decimal(17,14) DEFAULT NULL,
+  `y` decimal(16,14) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `gender` tinyint DEFAULT NULL,
+  `profile_image_url` varchar(255) DEFAULT NULL,
+  `role` enum('ADMIN','MEMBER') DEFAULT NULL,
+  `updated_at` datetime(6) NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `member_chk_1` CHECK ((`gender` between 0 and 2))
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- bunmo.product_category definition
+
+CREATE TABLE `product_category` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- bunmo.reported_user_history definition
+
+CREATE TABLE `reported_user_history` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `reported_date` date NOT NULL,
+  `reportee_id` bigint NOT NULL,
+  `reporter_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- bunmo.social_account definition
+
+CREATE TABLE `social_account` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `provider` enum('APPLE','KAKAO') NOT NULL,
+  `provider_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- bunmo.gathering definition
+
+CREATE TABLE `gathering` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `introduction` varchar(255) NOT NULL,
+  `open_chat_link` varchar(255) NOT NULL,
+  `gathering_time` datetime(6) NOT NULL,
+  `x` decimal(17,14) NOT NULL,
+  `y` decimal(16,14) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `max_participant_count` int NOT NULL,
+  `owner_id` bigint DEFAULT NULL,
+  `product_category` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKc4vg8y04aqb8e74jss5wvnpqo` (`product_category`),
+  CONSTRAINT `FKc4vg8y04aqb8e74jss5wvnpqo` FOREIGN KEY (`product_category`) REFERENCES `product_category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- bunmo.gathering_participant definition
+
+CREATE TABLE `gathering_participant` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `joined_at` datetime(6) NOT NULL,
+  `member_id` bigint DEFAULT NULL,
+  `gathering_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKmm1de6rmnptu6nd6qris5vy4g` (`gathering_id`),
+  CONSTRAINT `FKmm1de6rmnptu6nd6qris5vy4g` FOREIGN KEY (`gathering_id`) REFERENCES `gathering` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
